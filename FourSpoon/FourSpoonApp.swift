3902aeff4600805674b06ac68b648f5664ed822f
Rimesh Jotaniya <rimesh.j@icloud.com>
2025-08-19 10:56:47 +0530

    Initial Commit

---
+++ FourSpoon/FourSpoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourSpoonApp.swift
+//  FourSpoon
+//
+//  Created by Rimesh Jotaniya on 19/08/25.
+//
+
+import SwiftUI
+
+@main
+struct FourSpoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
